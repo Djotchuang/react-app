b5166003f00f71cc6c5155a656b8dbdea5f62877
Djotchuang <djotchuangtamo@gmail.com>
2021-05-18 15:10:55 +0100

    Markup Done - Finished Part 2 of the tutorial

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,33 +1,72 @@
 import React from "react";
-import TodosList from "./TodosList";
 import Header from "./Header";
+import InputTodo from "./InputTodo";
+import TodosList from "./TodosList";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuidv4(),
         title: "Learn Reactjs",
         completed: true,
       },
       {
-        id: 2,
+        id: uuidv4(),
         title: "Build and develop a project",
         completed: false,
       },
       {
-        id: 3,
+        id: uuidv4(),
         title: "Upload the project on github",
         completed: false,
       },
     ],
   };
 
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  deleteTodo = (id) => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter((todo) => {
+          return todo.id !== id;
+        }),
+      ],
+    });
+  };
+
   render() {
     return (
       <>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <InputTodo addTodoProps={this.addTodoItem} />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.deleteTodo}
+        />
       </>
     );
   }
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -4,7 +4,15 @@ class TodoItem extends React.Component {
   render() {
     return (
       <li>
-        <input type="checkbox" /> {this.props.todo.title}
+        <input
+          type="checkbox"
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        {this.props.todo.title}
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
       </li>
     );
   }
